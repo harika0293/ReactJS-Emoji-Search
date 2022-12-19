d4abae7ecdf588b80ffc27702cf3de7256d222ef
Harika Rachamalla <harika.0293@gmail.com>
2022-12-19 18:37:32 +0530

    Updates

---
+++ precache-manifest.ed53865b06fd8150583728dfa7e70c7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43defcbce462f1df1547306b26e2e309",
+    "url": "/ReactJS-Emoji-Search/index.html"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "/ReactJS-Emoji-Search/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "0979973ca6994a044a0e",
+    "url": "/ReactJS-Emoji-Search/static/css/main.340fb994.chunk.css"
+  },
+  {
+    "revision": "47cde07b2d6f77ce9b10",
+    "url": "/ReactJS-Emoji-Search/static/js/2.b81e05a8.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/ReactJS-Emoji-Search/static/js/2.b81e05a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0979973ca6994a044a0e",
+    "url": "/ReactJS-Emoji-Search/static/js/main.e2d8abac.chunk.js"
+  },
+  {
+    "revision": "902d266606ae36fac842",
+    "url": "/ReactJS-Emoji-Search/static/js/runtime-main.5a678fe3.js"
+  }
+]);
